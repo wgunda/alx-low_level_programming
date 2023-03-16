9fd5fb381bf4f87f56a7d899dc4e09218b3ab92b
wgunda <wgunda@gmail.com>
2023-03-16 06:39:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
+
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
- *  main - Entry
- *  Return: Always 0
-=======
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
->>>>>>> e82da641b13241a994a311aa96c381f6fc9aedc3
+ * main - A programm that prints a line wiith puts function
+ *
+ * Return: (0) (Success)
  */
+
 int main(void)
 {
-<<<<<<< HEAD
-puts("\"Programmingis like building a multilingual puzzle");
-=======
-puts("\"Programming is like building a multilingual puzzle");
->>>>>>> 666e057f0fb7b44933d42de4bb15a5c26ebb1e79
-return (0);
+	puts("\"Programming is like building a multilingual puzzle,");
+	return (0);
 }
